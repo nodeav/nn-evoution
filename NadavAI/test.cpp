c18968d207deb0c242d64b0c0f617446aba725dd
Noa Wiesel <noa.wiesel@gmail.com>
2023-01-22 20:28:21 +0200

    integrate gtest

---
+++ NadavAI/test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include "utils.h"
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+
+//  EXPECT_EQ(1, 0);
+}
+
+TEST(GetDistanceTest, sanity) {
+
+  EXPECT_EQ(getDistance({0, 1}, {0, 2}), 1.0);
+  EXPECT_EQ(getDistance({0, 0}, {3, 4}), 5.0);
+
+
+}
+
+
