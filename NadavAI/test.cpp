d894103ddea18863b00f5fa26c3aed46c886e1b6
Nadav Eidelstein <nadav@weka.io>
2023-01-23 17:12:24 +0200

    tests: add tests for Radian.between

--- NadavAI/test.cpp
+++ NadavAI/test.cpp
@@ -19,3 +19,15 @@ TEST(GetDistanceTest, sanity) {
 TEST(GetAngleTest, sanity) {
     EXPECT_EQ(getAngle({0, 1}, {0, 2}), Radian(M_PI));
 }
+
+TEST(RadianTest, between) {
+    Radian rad(0.5);
+    EXPECT_TRUE(rad.between(0, 1));
+    EXPECT_TRUE(rad.between(0.5, 0.5));
+    EXPECT_TRUE(rad.between(0., 0.5));
+    EXPECT_TRUE(rad.between(2*M_PI, 0.5));
+    EXPECT_TRUE(rad.between(2*M_PI-0.01, 0.5));
+    EXPECT_FALSE(rad.between(0., 0.49));
+    EXPECT_FALSE(rad.between(2*M_PI-0.01, 0.49));
+    EXPECT_FALSE(rad.between(0.51, 0.49));
+}
